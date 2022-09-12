3cd05660cc9190488e26a004cdedcc43d5350a6a
Kalleb Garcia <107278569+Beffy34@users.noreply.github.com>
2022-09-11 20:51:21 -0600

    Add files via upload

---
+++ arch1-project-zero-Beffy34/draw_shapes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "draw.h"
+
+/* Prints a size x size square whose left col is at startCol */
+void print_square(int leftCol, int size)
+{
+  int i, j;
+  int endCol = leftCol + size;
+  for (int row = 0; row < size; row++){
+    int col;
+    for (col = 0; col < leftCol; col++) putchar(' ');
+    for (       ; col < endCol;  col++) putchar('*');
+    putchar('\n');
+  }
+}
+
+// Prints a triangle of specified height whose left edge is at col leftCol.
+void print_triangle(int leftCol, int size)
+{
+  for (int row = 0; row <= size; row++) {
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('*');
+    putchar('\n');
+  }
+}
+//Prints a arrow of combined attribute from square and triangle
+void print_arrow()
+{
+  print_triangle(3, 5);
+  print_square(6, 5);
+}
---
+++ arch1-project-zero-Beffy34/uimain.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "draw.h"
+
+int main() 
+{
+  puts("Welcome!");
+
+  while (1) { // Infinite while loop
+
+    fputs("Select which shape you want to print (Triangle = t, Square = s, a = Arrow, Chars  11x16 = c, Chars 8x12 = d, Chars 5x7 = e) or 'q' to quit\n> ", stdout);
+    fflush(stdout);		/* stdout only flushes automatically on \n */
+    int c;
+    while ((c = getchar()) == '\n'); /* ignore newlines */
+    if (c == EOF)		     /* terminate on end-of-file */
+      goto done;
+
+    // Given the user answer, select which method to call
+    switch (c) {
+    case 't':
+      puts("You selected triangle:");
+      print_triangle(5, 7);
+      break;
+    case 's':
+      puts("You selected square:");
+      print_square(5, 5);
+      break;
+    case 'c':
+      puts("You selected chars:");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_11x16(c);
+      break;
+    case 'd':
+      puts("You selected chars f-8x12:");
+      for (char c = 'A'; c < 'D'; c++)
+    print_char_8x12(c);
+      break;
+    case 'e':
+      puts("You selected chars f-5x7:");
+      for (char c = 'A'; c < 'D'; c++)
+    print_char_5x7(c);
+      break;
+    case 'a':
+      puts("You selected arrow:");
+      print_arrow();
+      break;
+    case 'q':
+      puts("Bye!");
+      goto done; 		/* terminate */
+    case '\n':
+      break;
+    default:
+      printf("Unrecognized option '%c', please try again!\n", c);
+    }
+  }
+
+ done: // To exit from program
+  return 0;
+}
